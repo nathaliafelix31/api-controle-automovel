b48ebd6484228a0f207bdeae4a0b3f831db57541
nathaliafelix31 <nathy.felix.31@gmail.com>
2022-08-24 22:54:05 -0300

    add: CRUD Car

--- src/controllers/carController.js
+++ src/controllers/carController.js
@@ -20,4 +20,62 @@ router.post('/registerCar', async (req, res)=>{
     }
 });
 
+
+router.get('/', async (req, res) =>{ 
+    try{
+        const cars = await Car.find();
+
+        return res.send({cars});
+
+    }catch (err){
+        return res.status(400).send({error: 'Error loading cars'});
+    }
+});
+
+router.get('/:carId',async (req, res) =>{ 
+    try{
+        const car = await Car.findOne({_id:req.params.carId});
+
+        return res.send({car});
+
+    }catch (err){
+        return res.status(400).send({error: 'Error loading car'});
+    }
+});
+
+
+router.put('/:carId',async (req, res) =>{ 
+    try{
+        
+        const { licensePlateCar } = req.body;
+        const { modelCar } = req.body;
+        const { colorCar } = req.body;
+        
+
+        const newCar = await Car.findByIdAndUpdate(req.params.carId,{ 
+            licensePlateCar,
+            modelCar,
+            colorCar,
+
+        }, {new: true});
+
+        await newCar.save();
+
+        return res.send({newCar});
+    }catch (err){
+        return res.status(400).send({error: err.message});
+    }
+});
+
+router.delete('/:carId',async (req, res) =>{ 
+    try{
+        await Car.findByIdAndRemove(req.params.carId);
+
+        return res.send({success: 'Car Deleted Successfully'});
+    }catch (err){
+        return res.status(400).send({error: 'Error deleting car'});
+    }
+});
+
+
 module.exports = app => app.use('/car', router)
