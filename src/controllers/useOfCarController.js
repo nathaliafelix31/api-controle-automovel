a6d03e6746b197446bc2de30b9e7e6925b9b7b8a
nathaliafelix31 <nathy.felix.31@gmail.com>
2022-08-25 22:08:21 -0300

    add: create, update and search from register

--- src/controllers/useOfCarController.js
+++ src/controllers/useOfCarController.js
@@ -7,16 +7,54 @@ const router = express.Router();
 
 router.post('/registerUseOfCar', async (req, res)=>{
 
-
     try{
-      
-        const useOfCar = await UseOfCar.create(req.body);
 
-        return res.send({useOfCar});
+        const useOfCars = await UseOfCar.find({ $or:[ {motorist: req.body?.motorist || '' }, { car: req.body?.car || '' } ], $and:[{EndOfUseDate: null}]});
+
+
+        if(useOfCars.length > 0) return res.send({error: 'Car is already being used'});
+
+        const newUseOfCar = await UseOfCar.create(req.body);
+
+
+        return res.send({newUseOfCar});
       
     } catch (err){
         return res.status(400).send({error: 'Registration failed'});
     }
 });
+
+
+router.get('/', async (req, res) =>{ 
+    try{
+        const useOfCars = await UseOfCar.find().populate(['motorist', 'car']);
+
+        return res.send({useOfCars});
+
+    }catch (err){
+        return res.status(400).send({error: 'Error loading useOfCars'});
+    }
+});
+
+router.put('/filter',async (req, res) =>{ 
+    try{
+        const{EndOfUseDate} = req.body;
+
+        const useOfCar = await UseOfCar.findOne( {$and: [{motorist: req.body.motorist}, { car: req.body.car}, {EndOfUseDate: null}]});
+
+        if(useOfCar === null) return res.send({error: 'Car already finished or not used!'});
+
+        const finishUseOfCar = await UseOfCar.findByIdAndUpdate(useOfCar._id,{ 
+            EndOfUseDate,
+        }, {new: true});
+
+        await finishUseOfCar.save();
+
+        return res.send({finishUseOfCar});
+    }catch (err){
+        return res.status(400).send({error: 'Error finish useOfCar'});
+    }
+});
+
 
 module.exports = app => app.use('/useOfCar', router)
